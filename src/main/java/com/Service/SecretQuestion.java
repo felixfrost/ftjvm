3243d995b7a07c669d3abcba16e0991a1c12da4b
Veronica <vera_danko93@hotmail.com>
2022-03-30 16:34:33 +0200

    Question funkar, next/new fungerar ej

--- src/main/java/com/Service/SecretQuestion.java
+++ src/main/java/com/Service/SecretQuestion.java
@@ -1,22 +1,46 @@
 package com.Service;
 
 import lombok.*;
+import org.springframework.context.annotation.Bean;
 
-import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "secret")
+
 public class SecretQuestion {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String question;
-    private String correctAnswer;
-    private String[] incorrectAnswers;
+    private String correct_answer;
+    private String[] incorrect_answers;
+    private List<String> mixedSecretAnswers = new ArrayList<>();
+
+    public SecretQuestion(String question, String correct_answer, String[] incorrect_answers) {
+        this.question = question;
+        this.correct_answer = correct_answer;
+        this.incorrect_answers = incorrect_answers;
+    }
+
+    public List<SecretQuestion> findAll() {
+        List<SecretQuestion> all = new ArrayList<>();
+        all.add(new SecretQuestion("Vilka av dessa har varit gruppers projekt namn?", "SirQuizAlot, Bitter, IMFDB, SneakySnakey", new String[] {"SirQuizLot, Ditter, IMSDB, Sneakysnake", "CatVikingQuiz, Birdy Fake IMDB, SnakeClone", "JustAnotherQuiz, TwitterClone, BlatentRipOff, Snake... Just snake"}));
+
+        all.forEach(SecretQuestion::mixSecretAnswers);
+        return all;
+    }
+
+    public List<String> getMixedSecretAnswers() { return mixedSecretAnswers;}
+
+
+    public void mixSecretAnswers() {
+        mixedSecretAnswers.add(this.correct_answer);
+        mixedSecretAnswers.addAll(Arrays.asList(this.incorrect_answers));
+        Collections.shuffle(mixedSecretAnswers);
+    }
 }
